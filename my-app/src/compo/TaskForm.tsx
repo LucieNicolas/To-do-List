68de1fe32c5725e9cc17d7a945e91422b3ed7e47
LNICAH39 <lucie.nicolas.external@airbus.com>
2023-10-23 09:26:10 +0200

    add css to taskForm

--- my-app/src/compo/TaskForm.tsx
+++ my-app/src/compo/TaskForm.tsx
@@ -1,6 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React, { useState } from "react";
 import { Form } from "react-bootstrap";
+import "../style/TaskForm.css";
 
 interface TaskFormProps {
   onAddTask: (title: string, description: string) => void;
@@ -19,19 +20,21 @@ const TaskForm = ({ onAddTask }: TaskFormProps) => {
 
   return (
     <>
-      <div>
-        <Form onSubmit={handleNewTaskSubmit}>
+      <div className="form">
+        <Form onSubmit={handleNewTaskSubmit} className="task-form">
           <input
             type="text"
             placeholder="Title"
             value={newTaskTitle}
             onChange={(event) => setNewTaskTitle(event.target.value)}
+            className="title-input"
           />
           <input
             type="text"
             placeholder="Description"
             value={newTaskDescription}
             onChange={(event) => setNewTaskDescription(event.target.value)}
+            className="description-input"
           />
           <button type="submit" className="add-task-button">
             Add new task
