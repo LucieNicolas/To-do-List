c1b16db5ef845d68cc24bbca94f794058c544737
LucieNicolas <107399308+LucieNicolas@users.noreply.github.com>
2023-11-06 12:58:39 +0100

    [US6] : hook to store new task (#7)
    * add newTask to localStorage
    
    * Load tasks list from local storage
    
    ---------
    
    Co-authored-by: LNICAH39 <lucie.nicolas.external@airbus.com>

--- my-app/src/page/App.tsx
+++ my-app/src/page/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import TaskDetails from "../compo/TaskDetails";
 import TaskList, { Task } from "../compo/TaskList";
@@ -6,6 +6,14 @@ import { tasksListData } from "../data/data";
 
 function App() {
   const [tasksList, setTasksList] = useState<Task[]>(tasksListData);
+
+  // Load taskList from local storage
+  useEffect(() => {
+    const tasksListFromStorage = JSON.parse(
+      localStorage.getItem("tasksList") || "[]"
+    );
+    setTasksList(tasksListFromStorage);
+  }, []);
 
   const handleIsCompleted = (taskId: number) => {
     const updatedTasksList = tasksList.map((task) => {
